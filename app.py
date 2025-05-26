121e297773ccde8617751747b1dc06c3b790ce9c
FableV25 <sgtfablev@gmail.com>
2025-05-26 13:07:16 -0600

    may work

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 app = Flask(__name__)
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 for local dev
+    app.run(host='0.0.0.0', port=port)
 
 CORS(app)
 # Factores de emisión en kg CO2 por km
